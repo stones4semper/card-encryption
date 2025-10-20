fb471ea56dd9887f54f1aa848d8189242d468d97
Eloike David <eloike92@gmail.com>
2025-10-20 18:00:35 +0100

    working

--- api/src/conn.php
+++ api/src/conn.php
@@ -1,5 +1,6 @@
 <?php
-    require_once __DIR__."/env.php";
+    require_once dirname(__DIR__, 2)."/env.php";
+
     date_default_timezone_set('Africa/Lagos');
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
--- api/src/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db_name = "card_encrypt";
-    $db_user = "root";
-    $db_pass = "****";
-    $flw_secret = "FLWSECK_TEST-******-X";
-    $flw_encryption_key = "*****";
